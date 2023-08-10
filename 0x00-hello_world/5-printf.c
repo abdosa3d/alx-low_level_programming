1924d21ce656c9a21c2431c257057d64068a9616
abdosa3d <abdallahsaad511@gmail.com>
2023-08-11 02:37:11 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
- *main - Entre point 
+ * main - Entre point 
  *
- *Description: A C program that prints with prinif function 
+ * Description: A C program that prints with prinif function 
  *
- *Return: Alawys 0 (Success)
+ * Return: Alawys 0 (Success)
 */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
