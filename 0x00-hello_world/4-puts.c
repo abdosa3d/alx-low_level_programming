364c5689e119ea74eaefaaf85386ece1708ae79b
abdosaad <abdallahsaad511@gmail.com>
2023-08-11 02:19:22 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *Description: A C program that prints with put function
  *Return: Alawys 0 (Success)
 */
-int main(void\)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
