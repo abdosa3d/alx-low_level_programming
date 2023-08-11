254082bc7182212e12a831b9c726a7a0b1588b93
abdosa3d <abdallahsaad511@gmail.com>
2023-08-11 20:59:32 +0300

    p or n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%i is positive\n", n);
-	else if (n ==)
+	else if (n == 0)
 		printf("%i is zero\n", n);
 	else
-		prinf("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	return (0);
 }
