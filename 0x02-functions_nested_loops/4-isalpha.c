ad162386b563ef78d265a49c91d9e9ad12fb2bd6
abdosaad <abdallahsaad511@gmail.com>
2023-08-16 18:04:56 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,9 @@
 
 int _isalpha(int c) {
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-        return 1; // It's an alphabetic character
+        return 1;
     } else {
-        return 0; // It's not an alphabetic character
+        return 0; 
     }
 }
 
