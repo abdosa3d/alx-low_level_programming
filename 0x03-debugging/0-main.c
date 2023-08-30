4b4363410e781446dc4b4121b89a1b41141251fb
abdosaad <abdallahsaad511@gmail.com>
2023-08-30 03:41:42 +0300

    bug fixes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#nclude "main.h"
-=======
-#include"main.h"
->>>>>>> refs/remotes/origin/main
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -11,19 +7,10 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
-=======
 	int i;
 
-	i = 98 % 2;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
->>>>>>> refs/remotes/origin/main
 }
